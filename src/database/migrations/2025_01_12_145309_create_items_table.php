a5c1571d1b0f202fb1bfa4dbbc3a19b8f21dff47
akihiro shimoduma <a.shimodum@gmail.com>
2025-01-12 16:56:24 +0900

    設計書に基づいてマイグレーション

---
+++ src/database/migrations/2025_01_12_145309_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name', 255);
+            $table->text('description');
+            $table->unsignedInteger('price');
+            $table->tinyInteger('condition');
+            $table->string('brand', 255)->nullable();
+            $table->string('category', 100)->nullable();
+            $table->string('image_path', 255)->nullable();
+            $table->boolean('is_sold')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
