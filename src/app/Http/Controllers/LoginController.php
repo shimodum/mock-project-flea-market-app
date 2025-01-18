eb0f3b0d1d2c82c018011dc7ff1bd0ecbeaca24e
akihiro shimoduma <a.shimodum@gmail.com>
2025-01-18 19:06:32 +0900

    ログイン画面作成

--- src/app/Http/Controllers/LoginController.php
+++ src/app/Http/Controllers/LoginController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    // ログインフォームを表示する
+    // ログインフォームを表示
     public function showForm()
     {
         return view('auth.login');
     }
 
-    // ログイン処理を行う
+    // ログイン処理
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        //認証情報を使ってログインを試みる
+        if (Auth::attempt($credentials)) {
+            // 認証成功
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+    }
 
 }
