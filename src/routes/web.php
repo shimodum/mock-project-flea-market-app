2b6378df4f9a7222f88bdedb5842b704fa6696be
akihiro shimoduma <a.shimodum@gmail.com>
2025-01-18 19:06:32 +0900

    ログイン画面作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
 
-
 // 会員登録関連
 Route::get('/register', [RegisterController::class, 'showForm'])->name('register.form');
 Route::post('/register', [RegisterController::class, 'store'])->name('register');
@@ -13,7 +12,11 @@ Route::post('/register', [RegisterController::class, 'store'])->name('register')
 // ログイン関連
 Route::get('/login', [LoginController::class, 'showForm'])->name('login.form');
 Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-// プロフィール設定関連
-Route::get('/mypage/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::post('/mypage/profile', [ProfileController::class, 'update'])->name('profile.update');
+// 認証が必要なルートをグループ化
+Route::middleware('auth')->group(function () {
+    // プロフィール設定関連
+    Route::get('/mypage/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::post('/mypage/profile', [ProfileController::class, 'update'])->name('profile.update');
+});
