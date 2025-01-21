dde4d22b9ac5c82a7a1b5c79401e5c4916d58bec
akihiro shimoduma <a.shimodum@gmail.com>
2025-01-21 23:56:10 +0900

    全体ルーティング（仮）

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PurchaseController;
 
 // 会員登録関連
 Route::get('/register', [RegisterController::class, 'showForm'])->name('register.form');
@@ -14,9 +16,24 @@ Route::get('/login', [LoginController::class, 'showForm'])->name('login.form');
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
+// 認証不要なルート
+Route::get('/', [ItemController::class, 'index'])->name('items.index'); // 商品一覧
+Route::get('/item/{item_id}', [ItemController::class, 'show'])->name('items.show'); // 商品詳細
+
 // 認証が必要なルートをグループ化
 Route::middleware('auth')->group(function () {
     // プロフィール設定関連
     Route::get('/mypage/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/mypage/profile', [ProfileController::class, 'update'])->name('profile.update');
+
+    // 商品購入関連
+    Route::get('/purchase/{item_id}', [PurchaseController::class, 'create'])->name('purchase.create');
+    Route::post('/purchase/{item_id}', [PurchaseController::class, 'store'])->name('purchase.store');
+    Route::get('/purchase/address/{item_id}', [PurchaseController::class, 'editAddress'])->name('purchase.editAddress');
+    Route::post('/purchase/address/{item_id}', [PurchaseController::class, 'updateAddress'])->name('purchase.updateAddress');
+
+    // 商品出品関連
+    Route::get('/sell', [ItemController::class, 'create'])->name('items.create');
+    Route::post('/sell', [ItemController::class, 'store'])->name('items.store');
+
 });
