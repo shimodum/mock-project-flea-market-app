1c07e007fd952f58fc9e08c16427123176eb6d0e
akihiro shimoduma <a.shimodum@gmail.com>
2025-01-24 00:44:03 +0900

    プロフィール設定画面遷移

---
+++ src/resources/views/items/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/items.css') }}">
+@endsection
+
+@section('content')
+    <!-- 商品一覧画面の内容をここに書く -->
+@endsection
---
+++ src/resources/views/profile/edit.blade.php
@@ -0,0 +1,3 @@
+{{-- resources/views/profile/edit.blade.php --}}
+<h1>プロフィール設定画面</h1>
+<p>このページでプロフィールを設定する。</p>
