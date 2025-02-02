46fc39263df04f68ea0bfe3681cbe1a2f9a8ea25
akihiro shimoduma <a.shimodum@gmail.com>
2025-02-02 19:32:10 +0900

    商品出品画面と商品一覧画面（トップページ）修正

--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -1,4 +1,3 @@
-{{-- 商品一覧画面（トップ画面） --}}
 @extends('layouts.app')
 
 @section('css')
@@ -7,24 +6,28 @@
 @endsection
 
 @section('content')
-<div class="tabs">
-    <span>おすすめ</span>
-    <span>マイリスト</span>
-</div>
-
-<div class="item-list">
-    @foreach($items as $item)
-        <div class="item">
-            <a href="{{ route('items.show', $item->id) }}">
-                <img src="{{ asset($item->image_path) }}" alt="{{ $item->name }}">
-                <h3>{{ $item->name }}</h3>
-            </a>
-            <p>¥{{ number_format($item->price) }}</p>
-            <p>{{ $item->condition_label }}</p>
-            @if ($item->is_sold)
-                <p class="sold">Sold</p>
-            @endif
+<div class="container items-container">
+    <div class="tabs-container">
+        <div class="tabs">
+            <span class="active">おすすめ</span>
+            <span>マイリスト</span>
         </div>
-    @endforeach
+    </div>
+
+    <div class="item-list">
+        @foreach($items as $item)
+            <div class="item">
+                <a href="{{ route('items.show', $item->id) }}">
+                    <img src="{{ asset($item->image_path) }}" alt="{{ $item->name }}">
+                    <h3>{{ $item->name }}</h3>
+                </a>
+                <p>¥{{ number_format($item->price) }}</p>
+                <p>{{ $item->condition_label }}</p>
+                @if ($item->is_sold)
+                    <p class="sold">Sold</p>
+                @endif
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
